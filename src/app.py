b31f34c6688381dd997f933f41de34c1a83065c4
Fumaz <mail@fumaz.dev>
2021-01-20 14:51:20 +0100

    Initial commit

---
+++ src/app.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, redirect
+
+app = Flask(__name__)
+
+
+@app.route('/twitch')
+def twitch():
+    return redirect('https://twitch.tv/FumazDev')
+
+
+@app.route('/discord')
+def discord():
+    return redirect('https://discord.gg/kSqXdGpYVB')
+
+
+@app.route('/youtube')
+def youtube():
+    return redirect('https://www.youtube.com/channel/UCtSNeK8-HLYKsOW2x0J25DQ')
+
+
+@app.route('/patreon')
+def patreon():
+    return redirect('https://www.patreon.com/Fumaz')
+
+
+@app.route('/twitter')
+def twitter():
+    return redirect('https://twitter.com/FumazDev')
+
+
+@app.route('/texturepack')
+def texture_pack():
+    return redirect('https://s.fumaz.dev/JKYOY')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0')
